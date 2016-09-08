351df9b404df944eb1ba0e457f9c142c85612dd0
Ryan Williams <rpwilliams96@gmail.com>
2016-09-07 21:47:11 -0500

    Added apple functionality

--- snake.js
+++ snake.js
@@ -2,7 +2,7 @@
 * Title: snake.js
 * Description: A web version of the classic snake arcade game for CIS 580
 * Author: Ryan Williams
-* Last updated: 9.4.2016
+* Last updated: 9.7.2016
 */
 
 /* Global variables */
@@ -14,16 +14,23 @@ backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();  // timestamp 
 
-var initial_x = 0;
-var initial_y = 0;
-
+var snake;
+var snake_speed;
 var snake_width = 20;
 var snake_height = 20;
 
-var snake = [{x: initial_x, y: initial_y, width: snake_width, height: snake_height}];
-var snake_size = 40;
+var apple_width = 20;
+var apple_height = 20;
 
-var snake_speed = 2;
+var INITIAL_SIZE = 10;
+var INITIAL_X = backBuffer.width / 2;
+var INITIAL_Y = backBuffer.height / 2;
+
+var apple_x;
+var apple_y;
+var eaten_apple = false;
+
+var score;
 
 var input = 
 {
@@ -42,22 +49,52 @@ var input =
  */
 function init()
 {
+  snake_speed = 3;
+  snake = [{x: INITIAL_X, y: INITIAL_Y, width: snake_width, height: snake_height}]
   current_direction = "right";
-  draw_snake(snake_size);
+  grow_snake(INITIAL_SIZE, INITIAL_X, INITIAL_Y);
+  apple_x = Math.floor((Math.random() * 740) + 1);  // Random x value between 750 and 1
+  apple_y = Math.floor((Math.random() * 460) + 1);  // Random y value between 470 and 1
+  score = 0;
 }
 init();
 
-/**
- * @function draw_snake
- * Draws a snake depending on the size.
- * @param size
- * The size of the snake (use snake_size)
- */
-function draw_snake(size)
+/*
+ *@function spawn_apple
+ * Spawns a new apple
+*/
+function spawn_apple()
 {
-  for(i = 0; i < size; i++)
+  frontCtx.fillStyle = 'red';
+  frontCtx.fillRect(apple_x, apple_y, apple_width, apple_height);
+}
+
+/*
+ *@function spawn_snake
+ * Spawns a new snake
+ *@param {index} the position in the snake's body array
+*/
+function spawn_snake(index)
+{
+  backCtx.fillStyle = 'darkgreen';
+  backCtx.fillRect(snake[index].x, snake[index].y, snake_width, snake_height);
+
+  backCtx.strokeStyle = 'green';
+  backCtx.strokeRect(snake[index].x, snake[index].y, snake_width, snake_height);
+}
+
+/**
+ * @function grow_snake
+ * Draws a snake depending on the size.
+ * @param {length} The size of the snake 
+ * @param {snakeX} The snake head's x position
+ * @param {snakeY} The snake head's y position 
+ */
+function grow_snake(length, snakeX, snakeY)
+{
+  for(i = 0; i < length; i++)
   {
-    snake.push({x: i, y: 0, width: snake_width, height: snake_height})
+    snake.push({x: snakeX, y: snakeY, width: snake_width, height: snake_height});
   }
 }
 
@@ -92,14 +129,14 @@ function loop(newTime)
 function update(elapsedTime) 
 {
   move(snake_speed);
-  // TODO: Spawn an apple periodically
-  // TODO: Grow the snake periodically
-  // DONE: Move the snake
-  // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  // TODO: Determine if the snake has eaten an apple
-  // TODO: Determine if the snake has eaten its tail
-  // TODO: [Extra Credit] Determine if the snake has run into an obstacle
 
+  /* Check for a collision with the screen or with the snake's body by the snake's head */
+  if(check_collision(snake[0].x, snake[0].y, snake) || snake[0].x > 750 || snake[0].x < 0
+    || snake[0].y > 480 || snake[0].y < 0)
+  {
+   init(); // Restart the game without pausing
+  }
+  check_apple_collision();
 }
 
 /**
@@ -113,22 +150,101 @@ function render(elapsedTime)
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
   frontCtx.clearRect(0, 0, frontBuffer.width, frontBuffer.height);
   paint();
+  
 }
 
 /**
   * @function paint
-  * "Paints" a new instance of the snake
+  * "Paints" a new rendering of the canvas
   */
 function paint()
 { 
+  if(eaten_apple)
+  {
+    grow_snake(10, snake[0].x, snake[0].y);
+    snake_speed += 1;
+    eaten_apple = false;
+  }
   for(i = snake.length - 1; i >= 0; i--)
   {
-    backCtx.fillStyle = 'darkgrey';
-    backCtx.fillRect(snake[i].x, snake[i].y, 20, 20);
+    spawn_snake(i);   
+  }
+  spawn_apple();
+}
 
-    backCtx.strokeStyle = 'black';
-    backCtx.strokeRect(snake[i].x, snake[i].y, 20, 20);    
-  }  
+/**
+  * @function checkCollision 
+  * Checks for a collision from the snake's head.
+  * @param {x} The snake's head x position
+  * @param {y} The snake's head y position
+  * @param {array} The snake array
+  * @returns Boolean if there was a collision or not
+  */
+var check_collision = function(x, y, array)
+{
+  for(i = 1; i < array.length; i++)
+  {
+    if(array[i].x === x && array[i].y === y)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function check_apple_collision()
+{
+  var smallY;
+  var smallX;
+  var bigX;
+  var bigY;
+  if(snake[0].x >= apple_x)
+  {
+    bigX = snake[0].x;
+    smallX = apple_x;
+  }
+  else if(snake[0].x < apple_x)
+  {
+    smallX = snake[0].x;
+    bigX = apple_x;
+  }
+  if(snake[0].y >= apple_y)
+  {
+    bigY = snake[0].y;
+    smallY = apple_y;
+  }
+  else if(snake[0].x < apple_x)
+  {
+    smallY = snake[0].y;
+    bigY = apple_y;
+  }
+
+  var apple_distance = Math.abs(Math.sqrt(((bigX - smallX)*(bigX - smallX))
+   + ((bigY - smallY)*(bigY - smallY))));
+
+  if(apple_distance <= 20)
+  {
+    apple_x = Math.floor((Math.random() * 740) + 1);  // Random x value between 750 and 1
+    apple_y = Math.floor((Math.random() * 460) + 1);  // Random y value between 470 and 1
+    spawn_apple(apple_x, apple_y);
+    score++; 
+    document.getElementById('score').innerHTML = "Score: " + score;
+
+    var tail = 
+    {
+      x: snake[0].x,
+      y: snake[0].y
+    };
+
+    eaten_apple = true;
+  }
+  else
+  {
+    var tail = snake.pop();
+    tail.x = snake[0].x;
+    tail.y = snake[0].y;
+  }
+  snake.unshift(tail);
 }
 
 /**
@@ -156,6 +272,8 @@ function move(speed)
   }
 }
 
+
+
 window.onkeydown = function(event)
 {
   event.preventDefault();
@@ -169,7 +287,6 @@ window.onkeydown = function(event)
       if(current_direction != "down")
       {
         current_direction = "up";
-        console.log("up");
       }
       break;
     // LEFT
@@ -178,7 +295,6 @@ window.onkeydown = function(event)
       if(current_direction != "right")
       {
         current_direction = "left";
-        console.log("left");
       } 
       break;  
     // RIGHT
@@ -187,7 +303,6 @@ window.onkeydown = function(event)
       if(current_direction != "left")
       {
         current_direction = "right";
-        console.log("right");
       } 
       break;
     // DOWN
@@ -196,14 +311,10 @@ window.onkeydown = function(event)
       if(current_direction != "up") 
       {
         current_direction = "down";
-        console.log("down");
       }
       break;
   }
 }
-
-
-
 
 
 /* Launch the game */
